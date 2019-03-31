8c4e341b5532bded6037ca4609ff1e9329e55d90
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2019-03-31 16:22:49 +0000

    Setting up database with flask, adding first template for testing

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipe_hub'
+app.config["MONGO_URI"] = 'mongodb://admin:s040793@ds229186.mlab.com:29186/recipe_hub'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find())
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
