5401039f953d0db3ec572df5a180cf9ef2d85e55
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2019-04-25 11:02:10 +0000

    Add routing for pages, add basic navigation and home and login page layouts

--- app.py
+++ app.py
@@ -11,9 +11,37 @@ app.config["MONGO_URI"] = 'mongodb://admin:s040793@ds229186.mlab.com:29186/recip
 mongo = PyMongo(app)
 
 @app.route('/')
+def home():
+    return render_template("home.html",
+    recipes=mongo.db.recipes.find())
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+    
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
+
+@app.route('/categories')
+def categories():
+    return render_template("categories.html")
+
+@app.route('/add_category')
+def add_category():
+    return render_template("addcategory.html")
+    
+@app.route('/recipe')
+def recipe():
+    return render_template("recipe.html")
+
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html",
+    return render_template("getrecipes.html",
     recipes=mongo.db.recipes.find())
 
     
