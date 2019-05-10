f9233d16cf0c77176707c34057b84bfe6c58c585
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2019-05-10 05:16:19 +0000

    Add additional response testing

--- test_app.py
+++ test_app.py
@@ -10,10 +10,16 @@ class BasicTestCase(unittest.TestCase):
         response = indexTest.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    def test_leaderboard_route(self):
+    def test_login_route(self):
         """Route Testing for Login Page"""
-        leaderRoute = app.test_client(self)
-        response = leaderRoute.get('/login', content_type='html/text')
+        loginRoute = app.test_client(self)
+        response = loginRoute.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_route(self):
+        """Route Testing for Sign Up Page"""
+        signupRoute = app.test_client(self)
+        response = signupRoute.get('/signup', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
